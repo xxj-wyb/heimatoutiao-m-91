3d28bc625a4a23e8f98a497313cacf912f841f62
xxj <1339737544@qq.com>
2020-01-11 18:24:19 +0800

    使用vant

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 <!-- 这里面放 根组件 -->
   <div id="app">
+    <router-view></router-view>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant' // 引入 vant组件库
+import 'vant/lib/index.css' // 引入 vant 样式文件
+Vue.use(Vant) // 全局注册 :实际上调用了 Vant导出的对象的方法  即install方法; 一旦完成注册  在任意位置就可以使用 Vant组件库的组件
 
 Vue.config.productionTip = false
 
