66259dd12ff931b9942a289b53a61d342aa8a258
xxj <1339737544@qq.com>
2020-01-15 19:32:06 +0800

    使用tabs组件和封装article-list组件

---
+++ src/views/home/components/article-list.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- 这里面封装 上拉加载和下拉刷新的组件 -->
+
+  <!-- 这里注意 这个div设置了滚动条 目的是 给后面做 阅读记忆 留下伏笔 -->
+  <!-- 阅读记忆 => 看文章看到一半 滑到中部 去了别的页面 当你回来时 文章还在你看的位置 -->
+  <div class="scroll-wrapper">
+    <!-- 放置list组件  list组件可以实现上拉加载 -->
+    <van-list v-model="upLoading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+      <!-- <van-cell v-for="item in list" :key="item" :title="item" /> -->
+    </van-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'article-list',
+  data () {
+    return {
+      upLoading: false, // 是否开启上拉加载
+      finished: false // 是否已经完成全部的数据加载
+    }
+  },
+  methods: {
+    // 上拉加载方法
+    onLoad () {
+      console.log('开始加载数据')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,87 @@
 <template>
   <!-- 首页模块 -->
-  <div class="container">主页</div>
+  <div class="container">
+    <!-- 通过 swipeable 属性可以开启左右滑动切换标签页 -->
+    <van-tabs v-model="activeIndex" swipeable>
+      <van-tab :title="'标签' + item" v-for="item in 10" :key="item">
+        <article-list></article-list>
+      </van-tab>
+  </van-tabs>
+  <span class="bar_btn">
+      <van-icon name="wap-nav" />
+  </span>
+  </div>
 </template>
 
 <script>
+import ArticleList from './components/article-list'
 export default {
-  name: 'home' // 用devtools查看组件时，可以看到对应的name名称
+  name: 'home', // 用devtools查看组件时，可以看到对应的name名称
+  data () {
+    return {
+      activeIndex: 0 // 默认激活第0个标签
+    }
+  },
+  components: {
+    ArticleList // 局部注册组件
+  }
 }
-
 </script>
 
 <style lang="less" scoped>
+.van-tabs {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  /deep/ .van-tabs__wrap {
+    height: 36px;
+    padding-right: 36px;
+    .van-tab {
+      line-height: 36px;
+    }
+    .van-tabs__line {
+      background-color: #3296fa;
+      height: 2px;
+    }
+  }
+  /deep/ .van-tabs__content {
+    flex: 1;
+    overflow: hidden;
+  }
+  /deep/ .van-tab__pane {
+    height: 100%;
+    .scroll-wrapper {
+      height: 100%;
+      overflow-y: auto;
+    }
+  }
+}
+.bar_btn {
+  width: 36px;
+  height: 35px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  &::before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+    box-shadow: 0 0 10px #999;
+    transform: scale(1, 0.6);
+  }
+  .van-icon-wap-nav {
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    text-align: center;
+    line-height: 35px;
+    position: relative;
+    z-index: 1000;
+    &::before {
+      font-size: 20px;
+    }
+  }
+}
 </style>
