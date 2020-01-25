88f3a23e6688c9392a680945836fcaf2378b6c2f
xxj <1339737544@qq.com>
2020-01-25 16:09:53 +0800

    实现下拉刷新  onRefresh

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -100,16 +100,33 @@ export default {
 
     // 下拉刷新的方法
     // 下拉刷新的数据应该 添加到原有数据的头部,并且要关闭加载状态
-    onRefresh () {
-      // 模拟数据逻辑:触发下拉刷新
-      console.log('下拉刷新')
-      setTimeout(() => {
-        let arr = Array.from(Array(10), (value, index) => '追加' + (index + 1))
-        // 添加到头部方法： unshift()
-        this.articles.unshift(...arr)
-        this.downLoading = false // 关掉下拉状态
-        this.refreshSuccessText = `更新了${arr.length}条数据`
-      }, 1000)
+    async onRefresh () {
+      // // 模拟数据逻辑:触发下拉刷新
+      // console.log('下拉刷新')
+      // setTimeout(() => {
+      //   let arr = Array.from(Array(10), (value, index) => '追加' + (index + 1))
+      //   // 添加到头部方法： unshift()
+      //   this.articles.unshift(...arr)
+      //   this.downLoading = false // 关掉下拉状态
+      //   this.refreshSuccessText = `更新了${arr.length}条数据`
+      // }, 1000)
+
+      // 下拉刷新永远拉取的是最新的数据
+      const data = await getArticles({ channel_id: this.channel_id, timestamp: Date.now() })
+      this.downLoading = false // 关掉下拉状态
+      // 判断有没有最新数据
+      if (data.results.length) {
+        // 如果长度大于0 表示有数据
+        this.articles = data.results // 将历史数据全都覆盖掉
+        // 之前已经将上拉加载finished设置成true了,此时需要继续往下拉，就需要把原来的状态再次打开
+        this.finished = false
+        // 注意此时依然需要获取此次的历史时间戳
+        this.timestamp = data.pre_timestamp // 下拉刷新之后，再上拉加载时，会用到这个历史时间戳的
+        this.refreshSuccessText = `更新了${data.results.length}条数据`
+      } else {
+        //  如果没有数据更新  什么都不需要变化
+        this.refreshSuccessText = '已是最新数据'
+      }
     }
   }
 }
