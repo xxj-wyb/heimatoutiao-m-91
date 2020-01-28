4022780ace825db6fd98edeb5f4b262dc392527f
xxj <1339737544@qq.com>
2020-01-28 19:56:45 +0800

    搜索中心-历史记录

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -6,7 +6,8 @@
     <!-- 导航 -->
     <!-- 搜索框 -->
     <!-- 如果想自动过滤前后空格,可以给v-model一个修饰符 trim -->
-    <van-search v-model.trim="q" placeholder="请输入搜索关键词" shape="round" />
+    <!-- search事件：确定搜索时触发 -->
+    <van-search @search="onSearch" v-model.trim="q" placeholder="请输入搜索关键词" shape="round" />
     <!-- 有输入内容时  =>显示联想搜索， 没内容时  => 显示历史记录-->
     <!-- 联想搜索 -->
     <van-cell-group class="suggest-box" v-if="q" >
@@ -15,15 +16,22 @@
       </van-cell>
     </van-cell-group>
     <!-- 历史记录 -->
-    <div class="history-box" v-else>
+    <!-- 只有当有历史记录的时候 才显示整个历史的记录的盒子 -->
+    <div class="history-box" v-else-if="historyList.length">
       <div class="head">
         <span>历史记录</span>
-        <van-icon name="delete"></van-icon>
+        <!-- 清空所有历史 -->
+        <van-icon name="delete" @click="clear"></van-icon>
       </div>
       <van-cell-group>
-        <van-cell>
-          <a class="word_btn">电脑</a>
-          <van-icon class="close_btn" slot="right-icon" name="cross" />
+        <!-- 循环生成历史记录  item是惟一的 搜索历史记录不应该重复 -->
+        <!-- 根据历史去搜索=>点击历史记录时,跳到搜索结果,给历史搜索注册点击事件 -->
+        <van-cell  @click="toResult(item)" v-for="(item,index) in historyList" :key="item">
+          <!-- item就是搜索的关键字 -->
+          <a class="word_btn">{{item}}</a>
+          <!-- 给删除按钮注册删除事件 -->
+          <!-- 事件修饰符 事件名.stop 表示 阻止了事件冒泡 -->
+          <van-icon @click.stop="delHistory(index)"  class="close_btn" slot="right-icon" name="cross" />
         </van-cell>
       </van-cell-group>
     </div>
@@ -31,12 +39,44 @@
 </template>
 
 <script>
+const key = 'hm-91-toutiao-history' // 定义一个key，用来当做存储 本地历史记录的key
 export default {
   name: 'search',
   data () {
     return {
-      q: ''
+      q: '', // 查询内容
+      historyList: [] // 存放历史记录的数据
     }
+  },
+  methods: {
+    delHistory (index) {
+      this.historyList.splice(index, 1)// 删除对应的历史记录
+      localStorage.setItem(key, JSON.stringify(this.historyList))// 重新写入缓存
+    },
+    // 跳到搜索结果页面
+    toResult (text) {
+      this.$router.push({ path: '/search/result', query: { q: text } }) // 直接跳转到搜索结果界面
+    },
+    // 清空所有历史
+    clear () {
+      this.historyList = [] // 清空所有历史
+      localStorage.setItem(key, '[]') // 重新写入缓存
+    },
+    // 什么时候触发搜索： 回车/虚拟键盘搜索
+    onSearch () {
+      // 跳转到搜索结果之前 应该把当前的搜索关键字 写入到历史记录
+      if (!this.q) return // 如果q为空则没有必要往下走
+      // 如果不为空 要将this.q加入到历史记录中去
+      let obj = new Set(this.historyList)// 生成一个set变量用来接收历史关键字  new Set会自动去重
+      obj.add(this.q)
+      this.historyList = Array.from(obj) // Array.from(): 将set转回数组
+      localStorage.setItem(key, JSON.stringify(this.historyList)) // 重新写入缓存
+      //  触发搜索事件应该去搜索结果页面 而且 也要携带参数
+      this.$router.push({ path: '/search/result', query: { q: this.q } }) // 直接跳转到搜索结果界面
+    }
+  },
+  created () {
+    this.historyList = JSON.parse(localStorage.getItem(key) || '[]') // 短路表达式，读取本地化的内容
   }
 }
 </script>
