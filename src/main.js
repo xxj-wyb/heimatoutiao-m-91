de2f7af63539202be7020bbae0e65f010e3ad14e
xxj <1339737544@qq.com>
2020-02-14 00:24:14 +0800

    解决app中物理返回按键的问题

--- src/main.js
+++ src/main.js
@@ -22,3 +22,36 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+// 黑马头条就是H5, H5就是运行在浏览器中的，只不过运行在 5+ Runtime这个增强版浏览器里面,浏览器和 APP的物理按钮并没有进行关联, 也就是 点击手机返回按钮, 浏览器并不会进行返回
+// 对物理返回按键进行以下处理
+// 在main.js中添加如下代码  此代码是采用的 5+ Runtime中的plus对象
+document.addEventListener('plusready', function () {
+  var webview = window.plus.webview.currentWebview()
+  window.plus.key.addEventListener('backbutton', function () {
+    webview.canBack(function (e) {
+      if (e.canBack) {
+        webview.back()
+      } else {
+        // webview.close(); //hide,quit
+        // plus.runtime.quit();
+        // 首页返回键处理
+        // 处理逻辑：1秒内，连续两次按返回键，则退出应用；
+        var first = null
+        window.plus.key.addEventListener('backbutton', function () {
+          // 首次按键，提示‘再按一次退出应用’
+          if (!first) {
+            first = new Date().getTime()
+            setTimeout(function () {
+              first = null
+            }, 1000)
+          } else {
+            if (new Date().getTime() - first < 1500) {
+              window.plus.runtime.quit()
+            }
+          }
+        }, false)
+      }
+    })
+  })
+})
